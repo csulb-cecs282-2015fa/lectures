5f27d9f485022e4bc6f671f85da3201856e5eee3
nealterrell <neal.terrell@csulb.edu>
2015-09-10 15:30:47 -0700

    Initial commit.

---
+++ Pointers/Pointer Practice/practice.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+// Write the following functions to practice using pointers.
+
+
+// Swap: given pointers to two integers, swap the values of the variables
+// they point to.
+void Swap(int *a, int *b) {
+
+
+
+}
+
+// Ask the user to enter a positive amount of gunpowder and an angle from 
+// 0 to 90. Set the two pointers to those values.
+void GetGunpowderAndAngle(double *gun, double *angle) {
+
+
+
+
+}
+
+
+int main() {
+   int x = 10, y = 20;
+
+   // Show how to call swap, so that after the call, x is 20 and y is 10.
+
+
+
+
+
+   double g, a;
+   // Show how to call GetGunpowderAndAngle.
+
+
+
+
+
+}
